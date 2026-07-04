e3fef2ce5e15b33dda994d4edcc024c7112fed08
Riya Jain <jriya9090@gmail.com>
2026-07-04 17:17:53 +0530

    Update API URL for production deployment

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,15 +1,15 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "https://aidly-final-merge.onrender.com/api",
-// });
-
-// export default api;
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://aidly-final-merge.onrender.com/api",
 });
 
 export default api;
+
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:3000/api",
+// });
+
+// export default api;
